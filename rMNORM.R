9e1356eb8d74153964807c27793a4faf905d1073
Chanmin Kim <ylit777@gmail.com>
2018-02-07 12:04:12 -0500

    first commit

---
+++ rMNORM.R
@@ -0,0 +1,6 @@
+MNORM <- function (n = 1, mean = rep(0, d), varcov){
+    sqrt.varcov <-  chol(varcov)
+    d <- ncol(sqrt.varcov)
+    drop(mean + t(matrix(rnorm(n * d), d, n)) %*% sqrt.varcov)
+}
+  
